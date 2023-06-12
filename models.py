47557fb10742389bb9a314bac16d56eb6ba39bd3
fedebianciotto <fedebianciotto@gmail.com>
2023-06-12 16:49:12 -0300

    initial commit

---
+++ models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.db.models.fields import CharField, URLField
+from django.db.models.fields.files import ImageField
+
+class Project (models.Model):
+    titulo = CharField(max_length=100)
+    descripcion = CharField(max_length=250)
+    imagen = ImageField(upload_to="principal/imagenes/")
+    url= URLField(blank=True)
